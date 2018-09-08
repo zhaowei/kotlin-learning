1c4806c9740b19ea0cfdaad143ce3167f0d9444e
zhaowei <zhaowei.me@gmail.com>
2018-09-08 21:03:49 +0800

    condition expression

--- src/zhaowei/demo/c1.1.kt
+++ src/zhaowei/demo/c1.1.kt
@@ -13,6 +13,14 @@ fun printSum(a: Int, b:Int): Unit {
 
 }
 
+fun maxOf(a: Int, b:Int) : Int {
+    if (a > b) {
+        return a
+    } else {
+        return b
+    }
+}
+
 fun main(args: Array<String>) {
 
     println(sum(3, 5))
@@ -24,6 +32,8 @@ fun main(args: Array<String>) {
     val b=2
     val c: Int
     c=3
+
+    println("max of 0 and 42 is ${maxOf(0, 42)}")
 
 
 }
