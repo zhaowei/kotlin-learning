923fcd8e64cf65c7f14595291057b0d1e0a2129f
zhaowei <zhaowei.me@gmail.com>
2018-09-08 21:55:02 +0800

    using ranges

--- src/zhaowei/demo/c1.1.kt
+++ src/zhaowei/demo/c1.1.kt
@@ -86,4 +86,19 @@ fun main(args: Array<String>) {
     println(describe(2))
     println(describe("other"))
 
+    val x = 10
+    val y = 9
+    if (x in 1..y+1) {
+        println("fits in range")
+    }
+
+    val list = listOf("a", "b", "c")
+    if (-1 !in 0..list.lastIndex) {
+        println("-1 is out of range")
+    }
+
+    for (x in 1..5) {
+        print(x)
+    }
+
 }
